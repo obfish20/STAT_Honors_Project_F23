10a6f6bbe7a5517f3ce7a8bbb4a8c697cec86493
Oliver Fisher <obfish20@stlawu.edu>
2023-11-29 11:52:45 -0500

    Yes?

--- animation_code.R
+++ animation_code.R
@@ -35,13 +35,22 @@ get_interval_data <- function(date_choice, animal = "DEERWHITETAILED",
 }
 
 date_vec <- seq(ymd('2010-07-15'), ymd('2010-11-15'), by = "days")
+# how do these days work? But there is not data for this??
+
+#me 
+min_to_max <- seq(ymd('2021-07-03'), ymd('2024-08-28'), by = "days")
+
+
 
 get_interval_data(date_choice = "2022-08-28", animal = "DEERWHITETAILED",
                   plus_minus = 15)
 
+
+
 map_out <- map(date_vec, get_interval_data,
                animal = "DEERWHITETAILED",
                plus_minus = 15)
+
 map_comb <- pmap(map_out, bind_rows, .id = "id")
 
 full_df <- map_comb[[1]] |> as_tibble() |>
@@ -59,16 +68,35 @@ anim_base <- ggplot(data = full_df, aes(x = x, y = y, group = id)) +
   geom_rug(data = full_rug_df, aes(x = time_Rad, y = NULL,
                                    group = NULL)) +
   theme_minimal(base_size = 18) +
-  labs(x = "Time of Day") +
+  labs(x = "Time of Day", y = "Observation Density") +
   ylim(c(0, NA))
+
+## alt plot
+anim_circle <- ggplot(data = full_df, aes(x = x, y = y, group = id)) +
+  geom_line(colour = "blue", linewidth = 2) +
+  coord_polar() +
+  theme_minimal() +
+  scale_x_continuous(breaks = seq(0, 24, by = 1), expand = c(0, 0)) +
+  ylim(0,0.15) +
+  labs(x = 'Species', y = 'Observation Density')
+##   scale_x_continuous(limits = c(0,360)) +
+##  , binwidth = 15, boundary = -7.5
 
 library(gganimate)
 library(transformr)
-anim_base + transition_time(date_choice) +
-  labs(title = "{frame_time}") +
+anim_circle + transition_time(date_choice) +
+  labs(title = "Month-Day: {format(frame_time, '%m-%d')}") +
   shadow_mark(colour = "black", linewidth = 0.25, alpha = 0.15,
               exclude_layer = 2)
 
+
+anim_base + transition_time(date_choice) +
+  labs(title = "Month-Day: {format(frame_time, '%m-%d')}") +
+  shadow_mark(colour = "black", linewidth = 0.25, alpha = 0.15,
+              exclude_layer = 2)
+## remove year
+## write -up
+## explorietyb plot
 
 
 
